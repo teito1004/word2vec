d57a149c44992b285b2c8a7c95ecfaf81895bb76
mizobuchi <y.mizobuchi.1151@gmail.com>
2018-05-30 21:20:33 +0900

    idf値算出追加

--- word2vec_livedoor.py
+++ word2vec_livedoor.py
@@ -19,6 +19,7 @@ class livedoor_w2v:
         self.fn_out = [self.make_path(['./livedoor-news-data-wakati','{}.xml'.format(file_name[i])]) for i in np.arange(len(file_name))]
         self.fn_model = self.make_path(['./livedoor-news-data-model','word2vec.model'])
         self.fn_pkl = [self.make_path(['./livedoor-news-pkl','{}.pickle'.format(file_name[i])]) for i in np.arange(len(file_name))]
+        self.fn_tfidf = [self.make_path(['./livedoor-news-tfidf','{}.pickle'.format(file_name[i])]) for i in np.arange(len(file_name))]
         self.id = id
 
     def wakati(self,file_name_in,file_name_out):#分かち書きに変換して保存する
@@ -31,6 +32,11 @@ class livedoor_w2v:
             word = tagger.parse(line).split('\n')
             result = [x.split('\t')[0] for x in word]
             for x in result:
+                if '　' in x:
+                    x = x.replace('　','_')
+                if ' ' in x:
+                    x = x.replace(' ','_')
+
                 fo.write(x + ' ')
             fo.write('\n')
             line = fi.readline()
@@ -43,20 +49,6 @@ class livedoor_w2v:
         sentences = word2vec.PathLineSentences('./livedoor-news-data-wakati')
         model = word2vec.Word2Vec(sentences,sg = 1,size = 100,min_count = 1,window = 10,hs = 1,negative = 0)
         model.save(self.fn_model)
-
-    def mean_w2v_dic(self):#すべての単語についてベクトルを出力し、平均を求める
-        model = word2vec.Word2Vec.load(self.fn_model)
-        word_vec = np.array([model.wv[self.dic[0]]])
-        for i in np.arange(1,len(self.dic)):
-            try:
-                word_vec = np.vstack([word_vec,model.wv[self.dic[i]].reshape(1,100)])
-            except:
-                print('word_vec_ERROR! errorWord : {}'.format(self.dic[i]))
-                continue
-
-        self.word_vec_ave = np.mean(word_vec,axis=0)
-        
-        #self.word_vec = np.mean(np.array([model.wv[word] for word in self.dic]),axis=0)
 
     def mean_w2v(self,fname):#渡されたファイルの中にあるすべての単語についてベクトルを出力し、平均を求める
         number_count = 0
@@ -87,10 +79,10 @@ class livedoor_w2v:
         return vec_aveList
 
     def make_path(self,path):#パスの作成(引数pathリストに入っているものを接続する)
-        if not os.path.exists(path[0]):
-            os.makedirs(path[0])
-
         for i in np.arange(len(path)-1):
+            if not os.path.exists(path[i]):
+                os.makedirs(path[i])
+
             path[i+1] = os.path.join(path[i],path[i+1])
         return path[len(path)-1]
 
@@ -113,54 +105,46 @@ class livedoor_w2v:
             f.write('\n')
         f.close()
 
-    def make_dic(self,input_file,dic_file,flag):#辞書データ読み込み,追加,作成,更新
-        mecab = MeCab.Tagger("-Ochasen")
+    def make_dic(self,input_file,dic_file):#辞書データ読み込み,追加,作成,更新
         skip_strList = [' ','','　']
         if not os.path.isdir('data'):
             os.makedirs('data')
         #辞書データが作られて居なければ書き込みモードで起動してデータの入れ物を作成
         if not os.path.isfile(dic_file):
-            f = open(dic_file,'w')
-            self.dic = []
+            dic = []
         else:#作られて居たならば中身を読み込む
-            f = open(dic_file,'r')
-            self.dic = f.read().split('\n')
-        f.close()
+            dic = list(pd.read_csv(dic_file,header=None)[0].values)
 
-        if flag:
+        #idf値に必要なものの宣言
+        sentence_cnt = 0
+        word_sentence_cnt = np.zeros(len(dic))
+        word_allcnt = np.zeros(len(dic))
+
+        for fn in input_file:
             #xmlを整形して作ったtxtから単語を読み出す
-            f_in = open(input_file,'r')
-            word = []
+            f_in = open(fn,'r')
+            #記事ごとに分割したリストを作成
             buf = f_in.read().split('\n')
             for x in buf:
-                mor = mecab.parse(x).split('\n')
-                for w in mor:
-                    if word == []:
-                        word = list(w.split('\t')[0])
+                sentence_cnt+=1
+                #単語ごとに分割したリストを作成
+                mor = x.split(' ')
+                #記事内で重複要素を消したリストを作成
+                mor_list = list(set(mor))
+                for w in mor_list:
+                    if w in skip_strList:
+                        continue
+                    if w in dic:
+                        dic.append(w)
+                        np.append(word_allcnt,np.array([1]))
+                        np.append(word_sentence_cnt,np.array([1]))
                     else:
-                        word.append(w.split('\t')[0])
+                        word_allcnt[np.where(np.array(dic) == w)[0]]+= mor.count(w)
+                        word_sentence_cnt[np.where(np.array(dic) == w)[0]]+=1
             f_in.close()
-            count = 0
-            #辞書データの中にwordが含まれて居なかったら追加
-            for x in word:
-                if x in skip_strList:
-                    continue
-
-                if count % 10000 == 0:
-                    print('word:{}/{}'.format(count,len(word)))
-
-                if self.dic == []:
-                    self.dic = list(x)
-                else:
-                    df = pd.DataFrame(self.dic)
-                    if np.sum((df[0] == x).values) == 0:
-                        self.dic.append(x)
-                count+=1
-            #辞書データを改行区切りで書き出す
-            f_dict = open(dic_file,'w')
-            for x in self.dic:
-                f_dict.write(x + '\n')
-            f_dict.close()
+        idf = np.log((sentence_cnt/word_sentence_cnt))+1
+        df = pd.DataFrame(np.hstack([np.array(dic).reshape(-1,1),word_allcnt.reshape(-1,1),word_sentence_cnt.reshape(-1,1),idf.reshape(-1,1)]),columns = ['word','word_cnt_all','word_cnt_sentence','idf'])
+        df.to_csv(dic_file)
 
 if __name__ =="__main__":
     Data_flag = (sys.argv[1]=='T')
@@ -171,7 +155,7 @@ if __name__ =="__main__":
     #ファイル名がすべて接続されて居るので区切り文字を指定してファイル名ごとに分ける
     lines = f.readlines()
     fn = [line.strip() for line in lines]
-    dic_path = './data/dict.txt'
+    dic_path_list = ['./data/dict.csv']
     #オブジェクト作成
     w2v = livedoor_w2v(fn,len(fn))
     if Data_flag:
@@ -180,10 +164,11 @@ if __name__ =="__main__":
             print("outputfile:{}".format(w2v.fn_out[i]))
             w2v.xml_shaping(w2v.fn_in[i],w2v.fn_in_txt[i])
             w2v.wakati(w2v.fn_in_txt[i],w2v.fn_out[i])
-            if dict_flag:
-                w2v.make_dic(w2v.fn_in_txt[i],dic_path,Data_flag)
-    else:
-        w2v.make_dic(w2v.fn_in_txt[0],dic_path,False)
+
+    if dict_flag:
+        w2v.make_dic(w2v.fn_out,dic_path)
+
+    pdb.set_trace()
 
     if train_flag:
         w2v.w2v_train()
@@ -193,5 +178,3 @@ if __name__ =="__main__":
         fp = open(w2v.fn_pkl[i],'wb')
         pkl.dump(vec_ave,fp)
         fp.close()
-    #pk.dump(w2v.word_vec_ave,fp)
-    #fp.close()
