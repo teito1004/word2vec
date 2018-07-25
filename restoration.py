6bd94aecfcb2b5ecd560e1971c6a54eac2308ab0
mizobuchi <y.mizobuchi.1151@gmail.com>
2018-07-25 14:42:47 +0900

    仕様修正

--- restoration.py
+++ restoration.py
@@ -6,11 +6,21 @@ import pickle as pkl
 import os
 
 batch_size = 100
-batch_count = 0
+train_count = 0
+test_count = 0
 ite = 10000
 src_vec_length = 2
+hidden_unit_num = 200
 tar_vec_length = 100
 method = 2#1:w2v,2:tfidf
+train_per = 0.8
+
+
+def weight_variable(name,shape):
+    return tf.get_variable(name,shape,initializer=tf.random_normal_initializer(stddev=0.1))
+
+def bias_variable(name,shape):
+    return tf.get_variable(name,shape,initializer=tf.constant_initializer(0.1))
 
 #tensorflowで用いるデータ群作成
 input_data = tf.placeholder(shape=(None,None),dtype = tf.float32,name='input_data')
@@ -19,15 +29,15 @@ target_data = tf.placeholder(shape=(None,None),dtype = tf.float32,name='target_d
 #target_label = tf.placeholder(shape=(None),dtype = tf.int32,name = 'target_label')
 
 #線形回帰で必要なW,Bを作成
-W1 = tf.Variable(tf.random_uniform([src_vec_length,tar_vec_length],-1,1))
-B1 = tf.Variable(tf.random_uniform([tar_vec_length],-1,1))
-W2 = tf.Variable(tf.random_uniform([tar_vec_length,tar_vec_length],-1,1))
-B2 = tf.Variable(tf.random_uniform([tar_vec_length],-1,1))
+W1 = weight_variable('weight1',[src_vec_length,hidden_unit_num])
+B1 = weight_variable('bias1',[hidden_unit_num])
+W2 = weight_variable('weight2',[hidden_unit_num,tar_vec_length])
+B2 = weight_variable('bias2',[tar_vec_length])
 
 #活性化関数を指定してfc層を生成。
-fc1 = tf.sigmoid(tf.add(tf.matmul(input_data,W1),B1))
+fc1 = tf.nn.relu(tf.add(tf.matmul(input_data,W1),B1))
 #fc1 = tf.nn.relu(tf.add(tf.matmul(input_data,W1),B1))
-fc2 = tf.sigmoid(tf.add(tf.matmul(fc1,W2),B2))
+fc2 = tf.add(tf.matmul(fc1,W2),B2)
 #fc2 = tf.nn.relu(tf.add(tf.matmul(fc1,W2),B2))
 
 #loss関数(平均二乗誤差)
@@ -57,8 +67,12 @@ elif method == 2:
 fp = open(src_dataPath,'rb')
 src_data = pkl.load(fp)
 src_label = pkl.load(fp)
-randInd = np.random.permutation(src_label.shape[0])
-src_vec_length = src_data.shape[1]
+src_batch_size = src_data.shape[0]
+#train用、test用で分割する。
+src_train_data = src_data[:int(src_data.shape[0]*train_per)]
+src_test_data = src_data[int(src_data.shape[0]*train_per):]
+randInd_train = np.random.permutation(src_train_data.shape[0])
+randInd_test = np.random.permutation(src_test_data.shape[0])
 fp.close()
 
 #100次元のベクトルデータを読み込む
@@ -82,16 +96,35 @@ for fInd in np.arange(len(fn)):
 		else:
 			tar_data = np.vstack([tar_data,tmpX])
 			tar_label = np.hstack([tar_label,tmpY])
+#train用、test用で分割する。
+tar_train_data = tar_data[:int(tar_data.shape[0]*train_per)]
+tar_test_data = tar_data[int(tar_data.shape[0]*train_per):]
+
+
 
 #batch作成
-def next_batch(count,Ind):
+def next_batch_train(count,Ind):
     #epochが終了しているならばIndexを作り直す
-    if(src_label.shape[0] < (batch_size*(count+1))):
-        Ind = np.random.permutation(src_label.shape[0])
+    if(src_train_data.shape[0] < (batch_size*(count+1))):
+        Ind = np.random.permutation(src_train_data.shape[0])
         count = 0
     #batch_size分だけ取り出して、カウントを増やす
-    batch_src = src_data[Ind[batch_size*count:batch_size*(count+1)]]
-    batch_tar = tar_data[Ind[batch_size*count:batch_size*(count+1)]]
+    batch_src = src_train_data[Ind[batch_size*count:batch_size*(count+1)]]
+    batch_tar = tar_train_data[Ind[batch_size*count:batch_size*(count+1)]]
+    #辞書型にして返す
+    return {
+        input_data:batch_src,
+        target_data:batch_tar,
+    },count+1,Ind
+
+def next_batch_test(count,Ind):
+    #epochが終了しているならばIndexを作り直す
+    if(src_test_data.shape[0] < (batch_size*(count+1))):
+        Ind = np.random.permutation(src_test_data.shape[0])
+        count = 0
+    #batch_size分だけ取り出して、カウントを増やす
+    batch_src = src_test_data[Ind[batch_size*count:batch_size*(count+1)]]
+    batch_tar = tar_test_data[Ind[batch_size*count:batch_size*(count+1)]]
     #辞書型にして返す
     return {
         input_data:batch_src,
@@ -102,8 +135,17 @@ def next_batch(count,Ind):
 saver=tf.train.Saver()
 #学習の開始
 for step in np.arange(ite+1):
-    fd,batch_count,randInd = next_batch(batch_count,randInd)
+    fd,train_count,randInd_train = next_batch_train(train_count,randInd_train)
     step_loss = str(sess.run(loss,feed_dict=fd))
-    if step%500 == 0:
+    if step%100 == 0:
         print('step:{},loss:{}'.format(step,step_loss))
         saver.save(sess,'restration_model/model.ckpt',global_step=step)
+
+    if step%500 ==0:
+        fd,test_count,randInd_test = next_batch_test(test_count,randInd_test)
+        step_loss,predicted = sess.run([loss,fc2],feed_dict=fd)
+        print('---------test_sequence---------')
+        print('loss:{}'.format(str(step_loss)))
+        print('input ->:{}'.format(fd[input_data][0]))
+        print('predicted ->:{}'.format(str(predicted[0])))
+        print('-------------------------------')
