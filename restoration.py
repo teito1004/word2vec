e419f6a8fde1aa0ec2f91af05f00e4bb0155461e
Hirotaka Hachiya <hacchan@gmail.com>
2018-07-26 12:04:15 +0900

    add distance computation

--- restoration.py
+++ restoration.py
@@ -37,11 +37,11 @@ def bias_variable(name,shape):
 #===========================
 
 #===========================
-#tensorflowで用いるデータ群作成
+# tensorflowで用いるデータ群作成
 input_data = tf.placeholder(shape=(None,None),dtype = tf.float32,name='input_data')
 target_data = tf.placeholder(shape=(None,None),dtype = tf.float32,name='target_data')
 
-#線形回帰で必要なW,Bを作成
+# 線形回帰で必要なW,Bを作成
 W1 = weight_variable('weight1',[src_vec_length,hidden_unit_num])
 B1 = weight_variable('bias1',[hidden_unit_num])
 W2 = weight_variable('weight2',[hidden_unit_num,hidden_unit_num])
@@ -49,26 +49,26 @@ B2 = weight_variable('bias2',[hidden_unit_num])
 W3 = weight_variable('weight3',[hidden_unit_num,tar_vec_length])
 B3 = weight_variable('bias3',[tar_vec_length])
 
-#活性化関数を指定してfc層を生成。
+# 活性化関数を指定してfc層を生成。
 fc1 = fc_relu(input_data, W1, B1)
 #fc2 = fc_relu(fc1, W2, B2)
 fc_out = fc(fc1,W3,B3)
 #===========================
 
 #===========================
-#loss関数(平均二乗誤差)
+# loss関数(平均二乗誤差)
 loss = tf.reduce_mean(tf.square(target_data - fc_out))
 
-#optimizerの設定
-train_optimaizer = tf.train.AdamOptimizer(0.1).minimize(loss)
+# optimizerの設定
+train_optimaizer = tf.train.AdamOptimizer(0.01).minimize(loss)
 
-#初期化
+# 初期化
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 #===========================
 
 #===========================
-#使用する手法ごとにデータパスを作成する。
+# 使用する手法ごとにデータパスを作成する。
 if method == 1:
     src_dataPath = 'livedoor-news-data-TSNE/w2v/data_label.pickle'
     tar_dataPath = 'livedoor-news-data-pkl/w2v'
@@ -76,25 +76,31 @@ if method == 1:
 elif method == 2:
     src_dataPath = 'livedoor-news-data-TSNE/tfidf/data_label.pickle'
     tar_dataPath = 'livedoor-news-data-pkl/tfidf'
+#===========================
 
-#srcデータとtarデータで保存方法が違うため、それぞれを専用に読み込む部分を作成する。
 
-#T-SNEで２次元に削減したデータと正解ラベルのペアをpickleファイルから読み込む
+#===========================
+# T-SNEで２次元に削減したデータと正解ラベルのペアをpickleファイルから読み込む
 with open(src_dataPath,'rb') as fp:
 	src_data = pkl.load(fp)
 	src_label = pkl.load(fp)
 
+# データ点間距離の計算
+src_data1=np.tile(src_data,(src_data.shape[0],1,1))
+src_data2 = np.transpose(src_data1,[1,0,2])
+src_dist = np.sum(np.square(src_data1-src_data2),axis=2)
+
 src_batch_size = src_data.shape[0]
 
-#train用、test用で分割する。
+# train用、test用で分割する。
 src_train_data = src_data[:int(src_data.shape[0]*train_per)]
 src_test_data = src_data[int(src_data.shape[0]*train_per):]
 randInd_train = np.random.permutation(src_train_data.shape[0])
-fp.close()
 #===========================
 
+
 #===========================
-#100次元のベクトルデータを読み込む
+# word2vecの100次元のベクトルデータを読み込む
 # ファイル名（カテゴリに対応）をまとめているテキストからファイル名を読み出す
 f = open('file_name.txt','r')
 # ファイル名がすべて接続されて居るので区切り文字を指定してファイル名ごとに分ける
@@ -103,7 +109,6 @@ fn = [line.strip() for line in lines]
 flag = 0
 for fInd in np.arange(len(fn)):
 	tar_fulldataPath = os.path.join(tar_dataPath,'{}.pickle'.format(fn[fInd]))
-	#print(tar_fulldataPath)
 	with open(tar_fulldataPath,'rb') as fp:
 		tmpX = pkl.load(fp)
 		tmpY = np.ones(tmpX.shape[0])*fInd
@@ -115,24 +120,33 @@ for fInd in np.arange(len(fn)):
 		else:
 			tar_data = np.vstack([tar_data,tmpX])
 			tar_label = np.hstack([tar_label,tmpY])
-#train用、test用で分割する。
+			
+# データ点間距離の計算
+tar_data1=np.tile(tar_data,(tar_data.shape[0],1,1))
+tar_data2 = np.transpose(tar_data1,[1,0,2])
+tar_dist = np.sum(np.square(tar_data1-tar_data2),axis=2)
+
+
+# train用、test用で分割する。
 tar_train_data = tar_data[:int(tar_data.shape[0]*train_per)]
 tar_test_data = tar_data[int(tar_data.shape[0]*train_per):]
 #===========================
 
+pdb.set_trace()
+
 #===========================
-#batch作成
+# batch作成
 def next_batch_train(count,Ind):
     #epochが終了しているならばIndexを作り直す
     if(src_train_data.shape[0] < (batch_size*(count+1))):
         Ind = np.random.permutation(src_train_data.shape[0])
         count = 0
     
-    #batch_size分だけ取り出して、カウントを増やす
+    # batch_size分だけ取り出して、カウントを増やす
     batch_src = src_train_data[Ind[batch_size*count:batch_size*(count+1)]]
     batch_tar = tar_train_data[Ind[batch_size*count:batch_size*(count+1)]]
     
-    #辞書型にして返す
+    # 辞書型にして返す
     return {
         input_data:batch_src,
         target_data:batch_tar,
@@ -141,9 +155,9 @@ def next_batch_train(count,Ind):
 #===========================
 
 #===========================
-#過程のsave用に必要なクラスを宣言
+# 過程のsave用に必要なクラスを宣言
 saver=tf.train.Saver()
-#学習の開始
+# 学習の開始
 for step in np.arange(ite+1):
     fd,train_count,randInd_train = next_batch_train(train_count,randInd_train)
     step_loss = str(sess.run(loss,feed_dict=fd))
