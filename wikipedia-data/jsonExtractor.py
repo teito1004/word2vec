4ce3648888c11e245fd90f42f28edf04df7e7270
Hirotaka Hachiya <hhachiya@sys.wakayama-u.ac.jp>
2018-12-09 15:21:32 +0900

    add wikipedia

---
+++ wikipedia-data/jsonExtractor.py
@@ -0,0 +1,22 @@
+import json
+import pdb
+import numpy as np
+import pickle
+
+with open('jawiki.txt','rt') as fp:
+#with open('data.txt','rt') as fp:
+	jawiki = fp.readlines()
+
+
+titles = []
+texts = []
+
+for a in np.arange(len(jawiki)):
+	article = json.loads(jawiki[a])
+	
+	titles.append(article['title'])
+	texts.append(article['text'])
+
+with open('title_text.pkl','wb') as fp:
+	pickle.dump(titles,fp)
+	pickle.dump(texts,fp)	
